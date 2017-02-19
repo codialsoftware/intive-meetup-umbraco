a56351696d0dda1424e5685971c393d65de4c562
Wiktor Jaszczuk <wiktor.jaszczuk@smtsoftware.com>
2017-02-19 19:08:37 +0100

    sass files are compiled to css + prod optimisation

--- src/Intive.Meetup.Umbraco.Web/webpack.config.js
+++ src/Intive.Meetup.Umbraco.Web/webpack.config.js
@@ -1,26 +1,31 @@
 const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = makeWebpackConfig(); //this parenthesis are *REALLY* important for test
 
 function makeWebpackConfig() {
     const ENV = process.env.npm_lifecycle_event;
     const isTest = ENV === 'tests' || ENV === 'tests:watch';
+    const isProd = ENV === 'build:prod';
 
     const config = {};
 
     config.entry = isTest ? void 0 : {
-        app: './App_Frontend/js/app.js'
+        app: ['./App_Frontend/js/app.js', './App_Frontend/scss/app.scss']
     };
 
     config.output = isTest ? {} : {
         path: `${__dirname}/dist`,
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js'
+        filename: isProd ? '[name].[chunkhash].js' : '[name].bundle.js',
+        chunkFilename: isProd ? '[name].[chunkhash].js' : '[name].bundle.js'
     };
 
     if (isTest) {
         config.devtool = 'inline-source-map';
+    } else if (isTest) {
+        config.devtool = 'source-map';
     } else {
         config.devtool = 'eval-source-map';
     }
@@ -31,6 +36,49 @@ function makeWebpackConfig() {
                 test: /\.js/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                // ASSET LOADER
+                // Reference: https://github.com/webpack/file-loader
+                // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+                // Rename the file using the asset hash
+                // Pass along the updated reference to your code
+                // You can add here any file extension you want to get copied to your output
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: isTest ? 'null-loader' : ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true,
+                                modules: true,
+                                importLoaders: true,
+                                localIdentName: "[name]__[local]___[hash:base64:5]"
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                plugins: function () {
+                                    return [
+                                        autoprefixer
+                                    ];
+                                }
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }
         ]
     };
@@ -50,7 +98,8 @@ function makeWebpackConfig() {
         })
     }
 
-    config.plugins = [];
+    config.plugins = [
+    ];
 
     if (!isTest) {
         config.plugins.push(
@@ -60,8 +109,18 @@ function makeWebpackConfig() {
                 inject: 'body',
                 year: new Date().getFullYear()                   //we can pass variables to template and use templates,
                                                                  // by default ejs template is used
-            })
+            }),
+            new ExtractTextPlugin('css/[name].[contenthash].css')
         );
     }
+
+    if (isProd) {
+        config.plugins.push(
+            // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+            // Only emit files when there are no errors
+            new webpack.NoErrorsPlugin()
+        )
+    }
+
     return config;
 }
