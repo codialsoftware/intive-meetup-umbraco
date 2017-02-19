d49520527ba67dba6f8cabb446ed8703df693347
lporebski <leszek.porebski@smtsoftware.com>
2017-02-19 16:29:24 +0100

    Simple grid editor in ES5

---
+++ src/Intive.Meetup.Umbraco.Web/App_Plugins/GridEditors/01SimpleGE/simple-ge-grid-editor.js
@@ -0,0 +1,20 @@
+﻿(function(angular) {
+    'use strict';
+
+    angular.module('umbraco').controller(
+        'Intive.GridEditors.SimpleGE01',
+        [ '$scope', simpleGEAngularController ]);
+    
+    function simpleGEAngularController($scope) {
+        activate.call(this, $scope.control);
+    }
+
+    function activate(persistence) {
+        initializePersistence(persistence);
+        this.model = persistence.value;
+    }
+
+    function initializePersistence(persistence) {
+        persistence.value = persistence.value || {};
+    }
+})(angular);
