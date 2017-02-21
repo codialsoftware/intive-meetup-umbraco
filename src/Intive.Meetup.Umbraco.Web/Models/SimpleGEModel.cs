b681842194066eff1ed66ebcfd22f1a4226f596b
lporebski <leszek.porebski@smtsoftware.com>
2017-02-21 19:34:48 +0100

    Upgrade C# version to 6.0 Implement SimgleGE using a helper

---
+++ src/Intive.Meetup.Umbraco.Web/Models/SimpleGEModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Intive.Meetup.Umbraco.Web.Models
+{
+    public class SimpleGEModel
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+    }
+}
